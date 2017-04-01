517f3d675fbe7e2ee9a027e88cf2b3bed1bf65ab
giteeng <mateus@dalbinaco.com>
2017-04-01 20:03:26 -0300

    Adjust main columns

--- app/components/App.js
+++ app/components/App.js
@@ -1,18 +1,16 @@
 import React, { PropTypes } from 'react'
 import Home from './Home'
+import Sidebar from './Sidebar';
 
 const App = React.createClass({
   render () {
     return (
-      <div className="container">
+      <div className="container" style={{paddingTop:64}}>
         <div className="row">
-          <aside className="col-sm-12 col-md-3 sidebar">
-            <p className="text-uppercase"><strong>Mateus Dal Bianco</strong><br/>Desenvolvedor front-end</p>
-            <p className="text-uppercase"></p>
-          </aside>
-          <div className="col-sm-12 col-md-9">
+          <div className="col-12 col-lg-3"><Sidebar /></div>
+          <main className="col-12 col-lg-9">
             {this.props.children}
-          </div>
+          </main>
         </div>
       </div>
     )
