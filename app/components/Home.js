b5583107d782822f71bf61709fd2c145997a8756
giteeng <mateus@dalbinaco.com>
2017-04-01 20:11:14 -0300

    Update Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,13 @@
 import React, { PropTypes } from 'react'
+import Badges from './Badges.js'
 
 const Home = React.createClass({
   render () {
     return (
       <div>
-        <h1>Homezinea</h1>
+        <h1>Crio UIs e websites responsivos.</h1>
+        <h2>Projetos</h2>
+        <a href="http://tripby.org">TRIPBY.org</a>
       </div>
     )
   }
