8e0dbafa9cb6674dc0d57f76ca2eb766213b0fe8
giteeng <mateus@dalbinaco.com>
2017-04-01 16:53:55 -0300

    Build main layout

--- app/components/App.js
+++ app/components/App.js
@@ -4,11 +4,16 @@ import Home from './Home'
 const App = React.createClass({
   render () {
     return (
-      <div>
-        <header>
-          <h1>Headerzino</h1>
-        </header>
-        {this.props.children}
+      <div className="container">
+        <div className="row">
+          <aside className="col-sm-12 col-md-3 sidebar">
+            <p className="text-uppercase"><strong>Mateus Dal Bianco</strong><br/>Desenvolvedor front-end</p>
+            <p className="text-uppercase"></p>
+          </aside>
+          <div className="col-sm-12 col-md-9">
+            {this.props.children}
+          </div>
+        </div>
       </div>
     )
   }
