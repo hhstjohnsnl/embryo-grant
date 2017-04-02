2ad243091c151627f5437375a8d3f47bf46646b1
giteeng <mateus@dalbinaco.com>
2017-04-02 17:40:52 -0300

    Update .map to new syntax; include list keys

--- app/components/Badges.js
+++ app/components/Badges.js
@@ -2,15 +2,17 @@ import React, { PropTypes } from 'react'
 
 const Badges = React.createClass({
   render () {
-    const skills = this.props.skills.map( function(skill) { //yes, I don't like that fat arrow (:
-      return <li className="badge badge-pill badge-default">{skill}</li>
-    })
-    return (
-      <ul style={{margin: '1rem 0', padding: 0}}>
-        {skills}
-      </ul>
-    )
-  }
+    const skills = this.props.skills.map( (skill, index) =>
+    <li key={index} className="badge badge-pill badge-default">
+      {skill}
+    </li>
+  )
+  return (
+    <ul style={{margin: '1rem 0', padding: 0}}>
+      {skills}
+    </ul>
+  )
+}
 })
 
 export default Badges
--- app/components/Projects.js
+++ app/components/Projects.js
@@ -7,17 +7,19 @@ const projects = [
 
 const Projects = React.createClass({
   render () {
-    const listProjects = projects.map( function(project) { //yes, I don't like that fat arrow (:
-      return (
-        <li><a className="project" href={project.url}><img src={project.logo} alt={project.name} height="32"/></a></li>
-      )
-    })
-    return (
-      <ul style={{margin: '1rem 0', padding: 0, listStyleType: 'none'}}>
-        {listProjects}
-      </ul>
-    )
-  }
+    const listProjects = projects.map( (project, index) =>
+    <li key={index}>
+      <a className="project" href={project.url}>
+        <img src={project.logo} alt={project.name} height="32"/>
+      </a>
+    </li>
+  )
+  return (
+    <ul style={{margin: '1rem 0', padding: 0, listStyleType: 'none'}}>
+      {listProjects}
+    </ul>
+  )
+}
 })
 
 export default Projects
