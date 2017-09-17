ade7f8bb93034c912fc5f9cede75707441ecb7e0
dalbinaco <mateus@dalbinaco.com>
2017-09-17 19:40:51 -0300

    install ganalytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,18 @@ module.exports = {
     title: `Gatsby Default Starter`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`, 
-    { 
+    `gatsby-plugin-react-helmet`,
+    {
       resolve: `gatsby-plugin-sass`,
       options: {
         precision: 8,
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-92895808-1',
+      },
+    },
   ],
 }
