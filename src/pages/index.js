ddddfa7d50769aa74a9307f22ecab3cee1f99f76
Mateus Dal Bianco <mateus@dalbinaco.com>
2017-12-19 17:11:59 -0200

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,46 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Typist from 'react-typist';
 import Clients from '../components/Clients'
 import Badges from '../components/Badges'
 
+import 'react-typist/dist/Typist.css'
+
 const IndexPage = () => (
   <div>
-    <div className="separator d-md-none" />
-    <section>
-      <p className="lead">Escrevo interfaces precisas, interativas e consistentes</p>
-      <Badges skills={['JavaScript', 'React', 'Redux', 'Jest', 'webpack', 'SASS', 'Git', 'Node.js']} />
+    <section className="py-4 py-md-5">
+      <div className="d-flex align-items-end">
+        <h1 className="">
+        <Typist>
+          I build<br className="d-md-none"/> <strong>websites</strong>
+          <Typist.Backspace count={8} delay={1000}/>
+          <strong>webapps</strong>
+          <Typist.Backspace count={7} delay={1000}/>
+          <strong>beautiful UIs</strong>
+        </Typist>
+      </h1>
+      </div>
+      <button className="btn btn-primary mt-3" style={{fontSize: '1.1rem'}}>Let's talk!</button>
     </section>
     <div className="separator"></div>
     <section>
-      <h1>Clientes</h1>
-      <Clients clients={[
-        {name: "Quartzo Studio", url: "http://quartzostudio.com/", location: 'Juiz de Fora, BR'},
-        {name: "Softin Sistemas", url: "http://softin.com.br/", location: 'Joinville, BR'},
-        {name: "Lite Regal Education", url: "https://www.literegal.co.uk/", location: 'London, UK'},
-        {name: "Framenet Brasil", url: "http://www.ufjf.br/framenetbr/", location: 'Juiz de Fora, BR'},
-        {name: "UFJF", url: "http://ufjf.br/", location: 'Juiz de Fora, BR'},
-      ]}/>
+      <div className="row">
+        <div className="col-12 col-md-4">
+          <h3>📱</h3>
+          <h3>Responsive layouts</h3>
+          <p>Looks great on <strong>any screen</strong></p>
+        </div>
+        <div className="col-12 col-md-4">
+          <h3>🚀</h3>
+          <h3>Fast loading</h3>
+          <p><strong>90+ score</strong> guarantee on PageSpeed</p>
+        </div>
+        <div className="col-12 col-md-4">
+          <h3>🔌</h3>
+          <h3>Integrations</h3>
+          <p><strong>Any API integrations</strong> you need</p>
+        </div>
+      </div>
     </section>
   </div>
 )
