2e5fb90ba0b944c657d28de742e7cb58c2a8a401
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-02-15 19:57:13 -0200

    Update Badges content on /about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -16,7 +16,62 @@ const About = (props) => {
           {messages.description}
         </section>
         <h2>{messages.tech}</h2>
-        <Badges skills={['React', 'GraphQL', 'Apollo', 'Redux',  'Jest', 'webpack', 'SASS', 'Git', 'Node.js']} />
+        <Badges skills={[
+          {
+            icon: 'react',
+            name: 'React',
+            url: 'https://reactjs.org/',
+            color: '#61DAFB'
+          },
+          {
+            icon: 'redux',
+            name: 'Redux',
+            url: 'https://redux.js.org/',
+            color: '#61DAFB'
+          },
+          {
+            icon: 'nodejs',
+            name: 'Node.js',
+            url: 'https://nodejs.org/',
+            color: '#61DAFB'
+          },
+          {
+            icon: 'webpack',
+            name: 'webpack',
+            url: 'https://webpack.js.org/',
+            color: '#61DAFB'
+          },
+          {
+            icon: 'sass',
+            name: 'Sass',
+            url: 'https://sass-lang.com/',
+            color: '#61DAFB'
+          },
+          {
+            icon: 'gatsby',
+            name: 'Gatsby',
+            url: 'https://gatsbyjs.org/',
+            color: '#61DAFB'
+          },
+          {
+            icon: 'docker',
+            name: 'Docker',
+            url: 'https://gatsbyjs.org/',
+            color: '#61DAFB'
+          },
+          {
+            icon: 'git',
+            name: 'Git',
+            url: 'https://git-scm.com/',
+            color: '#61DAFB'
+          },
+          {
+            icon: 'graphql',
+            name: 'GraphQL',
+            url: 'https://graphql.org/',
+            color: '#61DAFB'
+          },
+        ]} />
       </div>
     )
   } return (<div />)
