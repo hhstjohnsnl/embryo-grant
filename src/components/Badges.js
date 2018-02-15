4be134dc6af6154abcc69d07e6834893aacbd718
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-02-15 19:56:58 -0200

    Update Badges component

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -3,12 +3,15 @@ import React, { PropTypes } from 'react'
 const Badges = React.createClass({
   render () {
     const skills = this.props.skills.map( (skill, index) =>
-    <li key={index} className="badge badge-pill badge-light">
-      {skill}
+    <li className="col-4 col-md-2 col-lg-1 mb-4" key={index}>
+      <a href={skill.url} className="d-flex flex-column align-items-center">
+        <img className="mb-2" style={{filter: 'invert(100%)'}} src={`https://unpkg.com/simple-icons@latest/icons/${skill.icon}.svg`} width="48" height="48" />
+        <p>{skill.name}</p>
+      </a>
     </li>
   )
   return (
-    <ul style={{margin: '1rem 0', padding: 0}}>
+    <ul className="row list-unstyled p-0 mt-4">
       {skills}
     </ul>
   )
