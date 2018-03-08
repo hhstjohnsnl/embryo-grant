775f26d34d949051394aede863d23e1cf04b7e93
Mateus Dal Bianco <mateus@dalbinaco.com>
2018-03-08 20:47:12 -0300

    Set cache expiral in ms

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.post('/mail', recaptcha.middleware.verify, mailgun)
 
 // server
 
-app.use(express.static(path.resolve('public'), {maxage: '1d'}));
+app.use(express.static(path.resolve('public'), {maxAge: 86400000}));
 
 const PORT = process.env.PORT || 3000;
 
