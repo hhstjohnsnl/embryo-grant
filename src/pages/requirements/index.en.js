46bbeff63214744f59716772e7861fa4d4eac48d
jthetzel <jthetzel@gmail.com>
2018-04-25 06:59:07 -0230

    Add eligibility text

--- src/pages/requirements/index.en.js
+++ src/pages/requirements/index.en.js
@@ -22,6 +22,15 @@ const messages = {
         entrepreneurship(MCE), Memorial University and Eastern Health, and is
         funded by the Atlantic Canada Opportunities Agency (ACOA)
       </p>
+      <ul>
+        <li>Applicants may compete as individuals, but are encouraged to create teams with a variety of different skill sets and strengths.</li>
+        <li>Applicants are required to have at least one relevant healthcare team member. The Hacking Health St. John’s chapter will support in finding a relevant healthcare team member if they do not already have one. </li>
+        <li>Individuals may participate in more than one team.</li>
+        <li>Successful applicants will have formed a non-for-profit or commercial corporation in the province of Newfoundland and Labrador, prior to receiving their award. The Hacking Health St. John’s chapter will provide guidance if required for the same. </li>
+        <li>Applicants must have a business idea with an element of innovation compared to the solutions that currently exist in the marketplace and/or medical sector.</li>
+        <li>Nonprofits may participate in the competition but must demonstrate income generation potential beyond traditional philanthropy.  While we recognize that startup funding and initial capital for operations may come from philanthropic funds/individual donations, we view a social enterprise – regardless of the type of entity formation – as designed to operate like a for-profit business, which must include a revenue-generating income stream in addition to traditional philanthropic sources (donations, grants, foundations, etc.). All entries in the Embryo Grant will be judged on their ability to become financially self-sustaining, that is, to generate revenue more than costs.</li>
+        <li>Eligibility will be determined at the sole discretion of the selection committee.</li>
+      </ul>
     </div>
   ),
   tech: 'Technologies',
