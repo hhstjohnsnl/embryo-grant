3cb26507b97b24ea7d09cc4da1d52cac37b24947
jthetzel <jthetzel@gmail.com>
2018-04-26 16:19:22 -0230

    Update prize, requirements text

--- src/pages/prizes/index.en.js
+++ src/pages/prizes/index.en.js
@@ -8,10 +8,6 @@ const messages = {
       <p className="lead">Pre-seed funding</p>
       <ul>
         <li>Up to three $15,000 prizes of funding are available. The number of recipients will depend on the quality of applicants and is at the discretion of the selection committee.</li>
-        <li>The funding will be awarded by NATI.</li>
-        <li>The funding winner, will receive funding on a reimbursement basis. And would need to incur expenses within the stipulated funding period. (for example after being awarded a funding of $15,000 in phase I, the awardee would have to incur the expense within the Phase I period between May 28 and July 31)</li>
-        <li>Expense claims need to be submitted within 45 days of the respective phase end date.</li>
-        <li>All expenses need to be pre-approved by NATI.</li>
       </ul>
       <p className="lead">Co-working space and mentorship</p>
       <ul>
--- src/pages/requirements/index.en.js
+++ src/pages/requirements/index.en.js
@@ -7,30 +7,29 @@ const messages = {
     <div>
       <p className="lead">Got an idea that solves a health related problem? Get the ball rolling with up to $45,000 in funding</p>
       <p>
-        The Embryo Grant helps anyone interested in developing a health-related
-        innovation, that has the potential to translate into a sustainable
-        enterprise (e.g. products or services that address issues related to
-        inefficiencies in the healthcare industry, increase the quality of care
-        of patients, etc.).
+        The Embryo Grant supports the development of health-related innovation that
+        deomnstrates potential to grow into a sustainable
+        enterprise.        
       </p>
       <p>
-        The Embryo Grant is a seed/early stage fu​​nd, under the Health
-        Innovation Initiative, which is a project to build a thriving medical
+        The Embryo Grant is a seed fund made available by the Health
+        Innovation Initiative.
+        The Health Innovation Iniitiative aims to build a thriving medical
         technology entrepreneurial ecosystem within the province of Newfoundland
-        and Labrador. The Health Innovation Initiative is co-led by Newfoundland
-        Association of Technology Industries (NATI), Memorial Centre for
-        entrepreneurship(MCE), Memorial University and Eastern Health, and is
-        funded by the Atlantic Canada Opportunities Agency (ACOA)
+        and Labrador. The Health Innovation Initiative is supported by Newfoundland
+        Association of Technology Industries, Memorial Centre for
+        entrepreneurship, Memorial University of Newfoundland, Eastern Health, and
+        the Atlantic Canada Opportunities Agency.
       </p>
       <p className="lead">Elgibility crieteria</p>
       <ul>
-        <li>Applicants may compete as individuals, but are encouraged to create teams with a variety of different skill sets and strengths.</li>
+        <li>Applicants may compete as individuals, but are encouraged to create teams with a variety of different skill sets and strengths.</li>
         <li>Applicants are required to have at least one relevant healthcare team member. The Hacking Health St. John’s chapter will support in finding a relevant healthcare team member if they do not already have one. </li>
         <li>Individuals may participate in more than one team.</li>
         <li>Successful applicants will have formed a non-for-profit or commercial corporation in the province of Newfoundland and Labrador, prior to receiving their award. The Hacking Health St. John’s chapter will provide guidance if required for the same. </li>
         <li>Applicants must have a business idea with an element of innovation compared to the solutions that currently exist in the marketplace and/or medical sector.</li>
-        <li>Nonprofits may participate in the competition but must demonstrate income generation potential beyond traditional philanthropy.  While we recognize that startup funding and initial capital for operations may come from philanthropic funds/individual donations, we view a social enterprise – regardless of the type of entity formation – as designed to operate like a for-profit business, which must include a revenue-generating income stream in addition to traditional philanthropic sources (donations, grants, foundations, etc.). All entries in the Embryo Grant will be judged on their ability to become financially self-sustaining, that is, to generate revenue more than costs.</li>
-        <li>Eligibility will be determined at the sole discretion of the selection committee.</li>
+        <li>Nonprofits may participate in the competition but must demonstrate income generation potential beyond traditional philanthropy. All entries in the Embryo Grant will be judged on their ability to become financially self-sustaining</li>
+        <li>Eligibility is determined at the discretion of the selection committee.</li>
       </ul>
     </div>
   ),
