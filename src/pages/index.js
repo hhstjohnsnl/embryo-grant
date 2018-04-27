b50843365f828b3380d262d482c9063930093886
jthetzel <jthetzel@gmail.com>
2018-04-27 07:43:16 -0230

    Fix null error in Typist

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ class IndexPage extends React.Component {
                   {messages.tagline.services.map((service, index) => (
                     <div className="d-inline" key={index}>
                       <strong>{service}</strong>
-                      {index === messages.tagline.services.length - 1 ? null : (
+                      {index === messages.tagline.services.length - 1 ? '' : (
                         <Typist.Backspace count={service.length} delay={1000} />
                       )}
                     </div>
