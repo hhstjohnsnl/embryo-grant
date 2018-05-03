d5f790fe9548cb9f1aab1b7206917f3d116cedd6
jthetzel <jthetzel@gmail.com>
2018-05-03 09:38:41 -0230

    Need one more emoji

--- src/pages/requirements/index.js
+++ src/pages/requirements/index.js
@@ -14,7 +14,7 @@ const Requirements = props => {
           <h1 style={{ alignSelf: 'flex-start' }}>{messages.title}</h1>
           <div style={{ flex: 1, justifyContent: 'flex-end'}}>
             <ul className="d-flex justify-content-end list-unstyled m-0 p-0" style={{ flexDirection: 'column'}} >
-              <li style={{ marginLeft: 'auto', padding: 0 }} >Questitons? Join us on <a href="https://slack.hhstjohnsnl.com">slack</a></li>
+              <li style={{ marginLeft: 'auto', padding: 0 }} >🤔 Questitons? Join us on <a href="https://slack.hhstjohnsnl.com">slack</a></li>
               <li style={{ marginLeft: 'auto', padding: 0 }} >or email stjohnsnl@hackinghealth.ca</li>
             </ul>
           </div>
